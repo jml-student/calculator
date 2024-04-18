f5a9867dfd919f7c111481b10b6bc036dfe3259d
jml-student <student741@proton.me>
2024-04-18 20:12:45 +0200

    Create files, main elements and styles

---
+++ script.js
@@ -0,0 +1,27 @@
+const container = document.querySelector(".container");
+const screen = document.querySelector(".screen");
+const buttons = document.querySelector(".buttons");
+
+
+
+
+
+function add() {
+
+}
+
+function substract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function operate() {
+
+}
